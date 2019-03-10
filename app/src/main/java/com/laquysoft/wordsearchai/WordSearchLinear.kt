00c7231626d917b0983ce7c4abd5241b1313c11d
Giovanni Laquidara <glaquidara@gmail.com>
2019-03-10 17:59:17 +0000

    Use directions

--- app/src/main/java/com/laquysoft/wordsearchai/WordSearchLinear.kt
+++ app/src/main/java/com/laquysoft/wordsearchai/WordSearchLinear.kt
@@ -7,6 +7,17 @@ import java.util.HashSet
 class WordSearchLinear {
     private var result: MutableSet<String> = HashSet()
 
+    private val directions = listOf(
+        Pair(0, 1),
+        Pair(0, -1),
+        Pair(1, 0),
+        Pair(-1, 0),
+        Pair(1, 1),
+        Pair(-1, -1),
+        Pair(1, -1),
+        Pair(-1, 1)
+    )
+
     fun findWords(board: Array<CharArray>, words: List<String>): List<String> {
 
         val trie = Trie()
@@ -16,20 +27,23 @@ class WordSearchLinear {
 
         board.forEachIndexed { rowIndex, row ->
             row.indices.forEach { charIndex ->
-                dfsT(board, visited, "", rowIndex, charIndex, trie)
-                dfsF(board, visited, "", rowIndex, charIndex, trie)
-                dfsD(board, visited, "", rowIndex, charIndex, trie)
-                dfsB(board, visited, "", rowIndex, charIndex, trie)
-                dfsFU(board, visited, "", rowIndex, charIndex, trie)
-                dfsFD(board, visited, "", rowIndex, charIndex, trie)
-                dfsBD(board, visited, "", rowIndex, charIndex, trie)
-                dfsBT(board, visited, "", rowIndex, charIndex, trie)
+                directions.forEach {
+                    dfs(board, visited, "", rowIndex, charIndex, trie, it)
+                }
             }
         }
         return ArrayList(result)
     }
 
-    private fun dfsF(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
+    private fun dfs(
+        board: Array<CharArray>,
+        visited: Array<BooleanArray>,
+        str: String,
+        i: Int,
+        j: Int,
+        trie: Trie,
+        direction: Pair<Int, Int>
+    ) {
         if (i < 0 || j < 0) return
 
         var str = str
@@ -54,213 +68,9 @@ class WordSearchLinear {
         }
 
         visited[i][j] = true
-        dfsF(board, visited, str, i, j + 1, trie)
+        dfs(board, visited, str, i + direction.first, j + direction.second, trie, direction)
         visited[i][j] = false
     }
 
-    private fun dfsB(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsB(board, visited, str, i, j - 1, trie)
-        visited[i][j] = false
-    }
-
-    private fun dfsT(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsT(board, visited, str, i+1, j, trie)
-        visited[i][j] = false
-    }
-
-    private fun dfsD(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsD(board, visited, str, i-1, j, trie)
-        visited[i][j] = false
-    }
-
-
-    private fun dfsFU(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsFU(board, visited, str, i-1, j+1, trie)
-        visited[i][j] = false
-    }
-
-    private fun dfsFD(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsFD(board, visited, str, i+1, j+1, trie)
-        visited[i][j] = false
-    }
-
-
-    private fun dfsBD(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsBD(board, visited, str, i+1, j-1, trie)
-        visited[i][j] = false
-    }
-
-    private fun dfsBT(board: Array<CharArray>, visited: Array<BooleanArray>, str: String, i: Int, j: Int, trie: Trie) {
-        if (i < 0 || j < 0) return
-
-        var str = str
-        val m = board.size
-
-        if (i >= m) return
-        val n = board[i].size
-
-        if (j >= n) return
-
-        if (visited[i][j])
-            return
-
-        str += board[i][j]
-
-        if (!trie.startsWith(str))
-            return
-
-        if (trie.search(str)) {
-            result.add(str)
-            Log.d("WORDSEARCH", "Result $str $i $j")
-        }
-
-        visited[i][j] = true
-        dfsBT(board, visited, str, i-1, j-1, trie)
-        visited[i][j] = false
-    }
 }
 
