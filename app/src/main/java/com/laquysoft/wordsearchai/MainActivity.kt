e85d3f58c07472fc3689755374cdd80e96cd5a47
joaobiriba <glaquidara@gmail.com>
2019-03-13 19:42:32 +0000

    Remove unused

--- app/src/main/java/com/laquysoft/wordsearchai/MainActivity.kt
+++ app/src/main/java/com/laquysoft/wordsearchai/MainActivity.kt
@@ -8,11 +8,8 @@ import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -104,23 +101,6 @@ class MainActivity : AppCompatActivity() {
         outState.putInt(KEY_IMAGE_MAX_WIDTH, imageMaxWidth)
         outState.putInt(KEY_IMAGE_MAX_HEIGHT, imageMaxHeight)
         outState.putString(KEY_SELECTED_SIZE, selectedSize)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun subscribeUi(adapter: WordListAdapter) {
